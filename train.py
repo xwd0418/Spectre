8487894c4de668c06bf53b1d9d63527b4e27b57d
James Zhao <jamezrollz@gmail.com>
2022-05-23 23:45:44 +0000

    image specs

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ def main():
     lr, epochs = args["lr"], args["epochs"]
 
     out_path = "/data/smart4.0"
-    path1, path2 = "lightning_logs", "pair"
+    path1, path2 = "transformer_experiments", "pair"
 
     model = PairNet(n_layers = 4, n_head = 4)
     data_module = PairDataModule(batch_size=32)
