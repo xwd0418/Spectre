3b7589b630b6cd2a72a36d24d73418ea05cb8025
James Zhao <jamezrollz@gmail.com>
2022-07-19 06:44:23 +0000

    added cli args for experiment names

--- train_ranker_transformer.py
+++ train_ranker_transformer.py
@@ -7,18 +7,26 @@ from pytorch_lightning.loggers import TensorBoardLogger
 from models.ranked_transformer import HsqcRankedTransformer
 from datasets.hsqc_folder_dataset import HsqcDataModule
 import logging, os, sys
+from datetime import datetime
+from pytz import timezone
+def get_curr_time():
+    pst = timezone("PST8PDT")
+    california_time = datetime.now(pst)
+    return california_time.strftime("%m_%d_%Y_%H:%M")
+
 
 def main():
     # dependencies: hyun_fp_data, hyun_pair_ranking_set_07_22
     parser = ArgumentParser(add_help=True)
     parser.add_argument("--epochs", type=int, default=120)
     parser.add_argument("--lr", type=float, default=1e-5)
+    parser.add_argument("--expname", type=str, default=f"experiment_{get_curr_time()}")
     args = vars(parser.parse_args())
 
     lr, epochs = args["lr"], args["epochs"]
 
     out_path = "/data/smart4.5"
-    path1, path2 = "lightning_logs", "hsqc_only_ranked"
+    path1, path2 = "lightning_logs", f"hsqc_only_ranked_4layers_4heads_{get_curr_time()}"
 
     model = HsqcRankedTransformer(lr=lr, n_layers=4, n_heads=4, dim_coords=(56, 56, 16))
     data_module = HsqcDataModule(batch_size=64)
