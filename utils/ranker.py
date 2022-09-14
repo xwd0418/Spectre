8a2505c962cf2fddd80b292f391cf7cb15d4cc68
James Zhao <jamezrollz@gmail.com>
2022-09-14 07:49:47 +0000

    wip tfidf

--- utils/ranker.py
+++ utils/ranker.py
@@ -2,7 +2,7 @@ import torch, torch.nn.functional as F, pickle
 from sklearn.preprocessing import normalize
 
 class RankingSet():
-    def __init__(self, store=None, file_path=None, retrieve_path=None,debug=False, device = "cuda"):
+    def __init__(self, store=None, file_path=None, retrieve_path=None, idf_weights=None, debug=False, device = "cuda"):
         '''
             Creates a ranking set. Assumes the file specified at file_path is a pickle file of 
             a numpy array of fingerprints.
@@ -11,15 +11,20 @@ class RankingSet():
         '''
         self.debug=debug
         self.device = device
-        if store is not None:
-            self.data = F.normalize(store, dim=1, p=2.0).to(device)
-        else:
-            with open(file_path, "rb") as f:
-                self.data = F.normalize(torch.load(f).type(torch.FloatTensor), dim=1, p=2.0).to(device)
-        
-        if retrieve_path:
-            with open(retrieve_path, "rb") as f:
-                self.lookup = pickle.load(f)
+        with torch.no_grad():
+            if store is not None:
+                self.data = F.normalize(store, dim=1, p=2.0).to(device)
+            else:
+                with open(file_path, "rb") as f:
+                    self.data = F.normalize(torch.load(f).type(torch.FloatTensor), dim=1, p=2.0).to(device)
+            
+            if retrieve_path:
+                with open(retrieve_path, "rb") as f:
+                    self.lookup = pickle.load(f)
+            if idf_weights is not None:
+                self.idf = idf_weights.to(device)
+                self.idf_data = F.normalize(self.data * self.idf, dim=1, p=2.0).to(device)
+
             
     @staticmethod
     def round(fp):
@@ -47,7 +52,16 @@ class RankingSet():
             nonzero = self.normalized_to_nonzero(self.data[idx])
             out.append(self.lookup.get(nonzero, None))
         return out
+    def dot_prod_rank(self, data, queries, truths, thresh):
+        with torch.no_grad():
+            q = queries.size()[0]
+            n = data.size()[0]
+            query_products = data @ queries.T # (n x 6144) * (6144 x q) = (n, q)
+            truth_products = data @ truths.T # (n, q)
 
+            ct_ones = torch.sum(torch.isclose(truth_products, torch.ones(n, q).to(self.device)), dim = 0) # (q)
+            ct_greater = torch.sum(query_products > thresh, dim = 0) # ((n, q) >= (q)) -> (q)
+            return ct_greater - ct_ones
     def batched_rank(self, queries, truths):
         '''
             Perform a batched ranking
@@ -59,16 +73,28 @@ class RankingSet():
         '''
         with torch.no_grad():
             q = queries.size()[0]
-            n = self.data.size()[0]
             queries = F.normalize(queries, dim=1, p=2.0).to(self.device)
             truths = F.normalize(truths, dim=1, p=2.0).to(self.device)
-
             thresh = torch.stack([torch.dot(queries[i,:], truths[i,:]) for i in range(q)]).to(self.device) # (q)
 
-            query_products = self.data @ queries.T # (n x 6144) * (6144 x q) = (n, q)
-            truth_products = self.data @ truths.T # (n, q)
+            return self.dot_prod_rank(self.data, queries, truths, thresh)
 
-            # we want to subtract off any samples in which the truth
-            ct_ones = torch.sum(torch.isclose(truth_products, torch.ones(n, q).to(self.device)), dim = 0) # (q)
-            ct_greater = torch.sum(query_products > thresh, dim = 0) # ((n, q) >= (q)) -> (q)
-            return ct_greater - ct_ones
+    def batched_rank_tfidf(self, queries, truths):
+        '''
+            Perform a batched ranking with tfidf
+
+            query(predictions) should be (q, 6144)
+            truths should be (q, 6144)
+
+            returns the number of items that have a higher dot product similarity (query_i (dot) truth_i)
+        '''
+        if self.idf_data is None:
+            raise Exception("No idf weights")
+        with torch.no_grad():
+            q = queries.size()[0]
+            queries = F.normalize(queries.to(self.device) * self.idf, dim=1, p=2.0)
+            truths = F.normalize(truths.to(self.device) * self.idf, dim=1, p=2.0)
+            thresh = torch.stack([torch.dot(queries[i,:], truths[i,:]) for i in range(q)]).to(self.device) # (q)
+            
+            return self.dot_prod_rank(self.idf_data, queries, truths, thresh)
+
