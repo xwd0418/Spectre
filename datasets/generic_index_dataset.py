5c5846b09204a36637868f588a884e00c40afd96
James Zhao <jamesjzhao01@gmail.com>
2023-02-15 21:41:18 +0000

    new indexed dataset

--- datasets/generic_index_dataset.py
+++ datasets/generic_index_dataset.py
@@ -12,11 +12,11 @@ class GenericIndexedDataset(Dataset):
       to ignore
     """
 
-    def __init__(self, dir, features, split="train"):
-        self.dir = dir
+    def __init__(self, direc, features, split="train", len_override = None):
+        self.dir = Path(direc)
         assert(split in ["train", "val", "test"])
 
-        self.split_dir = dir / split
+        self.split_dir = self.dir / split
         assert(os.path.exists(self.split_dir))
 
         self.features = features
@@ -25,8 +25,8 @@ class GenericIndexedDataset(Dataset):
         self.num_data = None
 
         for feature in features:
-            if os.path.exists(self.splitdir / feature / "index.pkl"):
-                with open(self.splitdir / feature / "index.pkl", "rb") as f:
+            if os.path.exists(self.split_dir / feature / "index.pkl"):
+                with open(self.split_dir / feature / "index.pkl", "rb") as f:
                    feats = pickle.load(f)
                    self.indexed_features[feature] = feats
                    if self.num_data is None:
@@ -37,33 +37,35 @@ class GenericIndexedDataset(Dataset):
 
         if self.num_data is None:
             raise Exception(f"[{str(self.__class__)}]: Could not interpolate dataset size!!!")
-    
+        
+        self.len_override = len_override
+ 
     def __len__(self):
-        return self.num_data
+        return self.num_data if self.len_override is None else self.len_override
 
     def __getitem__(self, idx):
         feats = [None] * len(self.features)
-        for idx, feature in enumerate(self.features):
+        for idx2, feature in enumerate(self.features):
             if feature not in self.indexed_features:
-                feats[idx] = torch.load(self.split_dir / feature / f"{idx}.pt")
+                feats[idx2] = torch.load(self.split_dir / feature / f"{idx}.pt")
             else:
-                feats[idx] = self.indexed_features[feature][idx]
+                feats[idx2] = self.indexed_features[feature][idx]
         return feats
 
 def build_collate_fn(feature_handlers):
     def collate(batch):
-      feat_columns = zip(*batch)
+      feat_columns = tuple(zip(*batch))
       out = [None] * len(feat_columns)
       for idx, (handler, feat_column) in enumerate(zip(feature_handlers, feat_columns)):
           if handler is None:
-              out[idx] = feat_column
+              out[idx] = torch.stack(feat_column, dim=0)
           else:
               out[idx] = handler(feat_column)
       return out
     return collate
 
 class GenericIndexedModule(pl.LightningDataModule):
-    def __init__(self, dir, features, feature_handlers, batch_size: int = 32, num_workers=4):
+    def __init__(self, dir, features, feature_handlers, batch_size: int = 32, num_workers=4, len_override=None):
         super().__init__()
         self.dir = dir
         self.features = features
@@ -71,13 +73,14 @@ class GenericIndexedModule(pl.LightningDataModule):
         self.batch_size = batch_size
         self.collate_fn = build_collate_fn(feature_handlers)
         self.num_workers = num_workers
+        self.len_override = len_override
     
     def setup(self, stage):
         if stage == "fit" or stage is None:
-            self.train = GenericIndexedDataset(self.dir, self.features, split="train")
-            self.val = GenericIndexedDataset(self.dir, self.features, split="val")
+            self.train = GenericIndexedDataset(self.dir, self.features, split="train", len_override=self.len_override)
+            self.val = GenericIndexedDataset(self.dir, self.features, split="val", len_override=self.len_override)
         if stage == "test":
-            self.test = GenericIndexedDataset(self.dir, self.features, split="test")
+            self.test = GenericIndexedDataset(self.dir, self.features, split="test", len_override=self.len_override)
         if stage == "predict":
             raise NotImplementedError("Predict setup not implemented")
 
