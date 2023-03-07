e03c2f6578ae08e09188f582ae52f22275a14b7d
James Zhao <jamesjzhao01@gmail.com>
2023-03-07 21:11:19 +0000

    wip cleaning code, adding torchmetrics

--- datasets/dataset_utils.py
+++ datasets/dataset_utils.py
@@ -1,9 +1,15 @@
-import numpy as np, torch
+import numpy as np
+import torch
 from torch.nn.utils.rnn import pad_sequence
 
 def pad(sequence):
   """
     Assume sequence is [batch, <whatever>]
   """
-  sequence = pad_sequence([torch.tensor(v, dtype=torch.float) for v in sequence], batch_first=True)
+  sequence = pad_sequence([
+      torch.tensor(v, dtype=torch.float32) if type(v) is not torch.Tensor
+      else v.type(torch.FloatTensor)
+      for v in sequence
+  ], batch_first=True)
+
   return sequence
