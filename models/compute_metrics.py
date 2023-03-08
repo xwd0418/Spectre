ef633545169fa98682906f575057c7085db99bda
James Zhao <jamezrollz@gmail.com>
2023-03-08 06:58:08 +0000

    best results logger, torch metrics

--- models/compute_metrics.py
+++ models/compute_metrics.py
@@ -1,34 +1,34 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from torchmetrics.classification import BinaryRecall, BinaryPrecision, BinaryF1Score
+from torchmetrics.classification import BinaryRecall, BinaryPrecision, BinaryF1Score, BinaryAccuracy
 
 do_cos = nn.CosineSimilarity(dim=1)
-do_f1 = BinaryF1Score()
-do_recall = BinaryRecall()
-do_precision = BinaryPrecision()
-do_accuracy = BinaryAccuracy()
+do_f1 = BinaryF1Score().cuda()
+do_recall = BinaryRecall().cuda()
+do_precision = BinaryPrecision().cuda()
+do_accuracy = BinaryAccuracy().cuda()
 
-def cm(model_output, fp_label, ranker, loss, loss_fn, thresh=0.0, device="cuda"):
+def cm(model_output, fp_label, ranker, loss, loss_fn, thresh: float = 0.0, device="cuda"):
   # Fingeprint prediction
-  fp_pred = model_output >= thresh
+  fp_pred = (model_output >= thresh).type(torch.cuda.FloatTensor)
 
   # cos
-  cos = do_cos(fp_label, fp_pred)
+  cos = torch.mean(do_cos(fp_label, fp_pred)).item()
   # bit activity
-  active = torch.mean(torch.sum(fp_pred, axis=1))
+  active = torch.mean(torch.sum(fp_pred, axis=1)).item()
   # bit metrics
-  f1 = do_f1(fp_pred, fp_label)
-  precision = do_precision(fp_pred, fp_label)
-  recall = do_recall(fp_pred, fp_label)
-  accuracy = do_accuracy(fp_pred, fp_label)
+  f1 = do_f1(fp_pred, fp_label).item()
+  prec = do_precision(fp_pred, fp_label).item()
+  rec = do_recall(fp_pred, fp_label).item()
+  acc = do_accuracy(fp_pred, fp_label).item()
 
-  if torch.isclose(thresh, 0.5):  # probabiltiies
+  if np.isclose(thresh, 0.5):  # probabiltiies
     pos_contr = torch.where(
         fp_label == 0, torch.zeros_like(fp_label, dtype=torch.float), model_output)
     neg_contr = torch.where(
         fp_label == 1, torch.ones_like(fp_label, dtype=torch.float), model_output)
-  elif torch.isclose(thresh, 0.0):  # logits
+  elif np.isclose(thresh, 0.0):  # logits
     pos_contr = torch.where(
         fp_label == 0, -999 * torch.ones_like(fp_label, dtype=torch.float), model_output)
     neg_contr = torch.where(
@@ -43,7 +43,6 @@ def cm(model_output, fp_label, ranker, loss, loss_fn, thresh=0.0, device="cuda")
   rank_res = ranker.batched_rank(fp_pred, fp_label)
   cts = [1, 5, 10]
   # strictly less as batched_rank returns number of items STRICTLY greater
-  print(rank_res)
   ranks = {
       f"rank_{allow}": torch.sum(rank_res < allow).item() / torch.numel(rank_res)
       for allow in cts
@@ -54,12 +53,12 @@ def cm(model_output, fp_label, ranker, loss, loss_fn, thresh=0.0, device="cuda")
       "pos_loss": pos_loss.item(),
       "neg_loss": neg_loss.item(),
       "pos_neg_loss": (pos_loss + neg_loss).item(),
-      "cos": torch.mean(cos).item(),
-      "active_bits": active.item(),
-      "f1": np.mean(f1),
-      "precision": np.mean(precision),
-      "recall": np.mean(recall),
-      "accuracy": np.mean(accuracy),
+      "cos": cos,
+      "active_bits": active,
+      "f1": f1,
+      "precision": prec,
+      "recall": rec,
+      "accuracy": acc,
       "mean_rank": mean_rank,
       **ranks
   }
