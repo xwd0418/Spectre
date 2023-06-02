28cd6d638ff73da70e47e3453cc57ca126818e0a
James Zhao <jamesjzhao01@gmail.com>
2023-06-02 18:05:29 +0000

    working for identity

--- models/smart_clip.py
+++ models/smart_clip.py
@@ -23,7 +23,10 @@ class SMART_CLIP(pl.LightningModule):
     smart_args = {k: kwargs[k] for k in SMART_ENCODER_ARGS if k in kwargs}
     self.my_logger = get_logger()
     # === Parameters ===
-    self.save_hyperparameters({**clip_args, **smart_args})
+    self.hparams = {**clip_args, **smart_args}
+
+    self.save_hyperparameters(self.hparams, logger=False)
+
     self.bart = BART_Encoder.load_from_checkpoint(chemformer_path, strict=False)
     self.smart = SMART_Encoder(**kwargs)
 
@@ -93,6 +96,14 @@ class SMART_CLIP(pl.LightningModule):
         "smiles_rankn": smiles_rankn.item()
     }
     self.validation_step_outputs.append(metrics)
+
+  def on_train_start(self) -> None:
+    self.my_logger.info(self.trainer.global_step)
+    if self.trainer.global_step == 0:
+      self.my_logger.info("Global step = 0, initializing hp_metric")
+      self.logger.log_hyperparams(self.hparams, {"hp_metric": -1})
+    else:
+      self.my_logger.info("Global step != 0, not initializing hp_metric")
 
   def on_train_epoch_end(self):
     if self.training_step_outputs:
