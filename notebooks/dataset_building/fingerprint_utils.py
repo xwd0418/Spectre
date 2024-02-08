b61c64e8820975a1baec1b4eaa0593ca54c84a71
Wangdong <w6xu@ucsd.edu>
2024-02-08 22:35:37 +0000

    change to cleaned dataset by inchi and using testing ranker during test

--- notebooks/dataset_building/fingerprint_utils.py
+++ notebooks/dataset_building/fingerprint_utils.py
@@ -12,14 +12,14 @@ def FP_generator(SMILES,radi): # radi is radius for morgan fingerprint
         mol_fp_H = rdMolDescriptors.GetMorganFingerprintAsBitVect(mol_H, radius=r, bitInfo=mol_bi_H, nBits = 2048)
         mol_bi_H_QC = []
         for i in mol_fp_H.GetOnBits():
-            idx = mol_bi_H[i][0][0]
+            # idx = mol_bi_H[i][0][0]
             radius_list = []
             for j in range(len(mol_bi_H[i])):
                 atom_radi = mol_bi_H[i][j][1]
                 radius_list.append(atom_radi) 
-            atom = mol_H.GetAtomWithIdx(idx)
-            symbol = atom.GetSymbol()
-            neigbor = [x.GetAtomicNum() for x in atom.GetNeighbors()]
+            # atom = mol_H.GetAtomWithIdx(idx)
+            # symbol = atom.GetSymbol()
+            # neigbor = [x.GetAtomicNum() for x in atom.GetNeighbors()]
             if r in radius_list: #and symbol == 'C' and 1 in neigbor:#radius = 2, atom = Carbon, H possessed Carbon
                 mol_bi_H_QC.append(i)
         bits = mol_bi_H_QC
