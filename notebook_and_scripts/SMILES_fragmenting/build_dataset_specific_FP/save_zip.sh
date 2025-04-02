66f6b11e17d8efddd4e7e474f4dba7fc818556f3
Wangdong <xwd0418@gmail.com>
2025-04-02 01:43:08 +0000

    shift to rd-kit FP

--- notebook_and_scripts/SMILES_fragmenting/build_dataset_specific_FP/save_zip.sh
+++ notebook_and_scripts/SMILES_fragmenting/build_dataset_specific_FP/save_zip.sh
@@ -1,4 +1,4 @@
-python /root/gurusmart/MorganFP_prediction/reproduce_previous_works/Spectre/notebook_and_scripts/SMILES_fragmenting/build_dataset_specific_FP/find_most_frequent_frags.py
+python /root/gurusmart/MorganFP_prediction/reproduce_previous_works/Spectre/notebook_and_scripts/SMILES_fragmenting/build_dataset_specific_FP/find_frags.py
 
 cd /workspace 
 # zip -r Spectre_all_frag_DB_Count_exists.zip SMILES_dataset OneD_Only_Dataset  *_radius_under_10.pkl 
